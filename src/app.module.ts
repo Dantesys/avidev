a090e9af87c9619bd82db0c169c765bccd31964b
Dantesys <hotuios@hotmail.com>
2021-10-08 16:17:21 -0300

    Configurando bd do heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,11 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: "localhost",
+      host: "ec2-54-208-96-16.compute-1.amazonaws.com",
       port: 5432,
-      username: "dantesys",
-      password: "diegohotuios",
-      database: "avdb",
+      username: "wdvdxtvrsrsggp",
+      password: "d5fa886af6af30b7a725b71a3f8ed658c1648a0f2209be6e118ebf83646cc2e5",
+      database: "d59soei61um0s9",
       autoLoadEntities: true,
       synchronize: true
     }),
